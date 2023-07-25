0aa09fa1bce0a59ec6f22c9d5c8432ade0e2be84
kakukouki <kakukouki132@gmail.com>
2023-07-25 11:49:01 +0900

    web

---
+++ ghjHTML/Hello/HelloWorld.js
@@ -0,0 +1,13 @@
+// var int1 = 10;
+// var str1 = "10";
+// alert(int1 = str1);
+// alert(int1 === str1);
+// var reg = /^\s*$/;
+// var flag = reg.test(" ");
+// alert(flag);
+
+for (let index = 0; index < 9; index++) {
+    
+console.log(index);
+    
+}
